e49207224de9f34f667d40f1eddbded1997fda8a
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2023-08-13 20:36:17 +0900

    -[오승담]2023-08-13
    - 404 Error Handling (설정된 경로로 controller를 맵핑하지 않아 발생한 문제)

--- Com2usProject/Program.cs
+++ Com2usProject/Program.cs
@@ -1,26 +1,36 @@
+using Com2usProject.Service;
+using Microsoft.AspNetCore.Builder;
 using ZLogger;
 
 var builder = WebApplication.CreateBuilder(args);
 
+IConfiguration appConfig = builder.Configuration;
+
+builder.Services.Configure<DbConnectionStrings>(appConfig.GetSection(nameof(DbConnectionStrings)));
 
 // 로그 설정
 builder.Host.ConfigureLogging(logging =>
 {
     logging.ClearProviders();
-    logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
     logging.AddZLoggerConsole();
 });
 
 // Add services to the container.
 
+builder.Services.AddTransient<IAccountDb, MySqlAccountDb>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
+}
+
+app.MapControllers();
 
 app.Run();
