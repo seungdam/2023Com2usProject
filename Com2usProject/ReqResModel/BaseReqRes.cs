0f5f871731001261d163d5905f13ceb1a2d8c2a6
DESKTOP-4NDCLE1\OH.S.D <erfrr@naver.com>
2023-08-25 23:51:48 +0900

    [오승담] 08-21~ 08-23
    - 불필요한 데이터 제거 http 항목
    - 일부 클래스명 및 파일명 변경
      AccountController.cs -> RegisterAccountController.cs
    - Service에서 Repository로 네임스페이스 변경
    -

--- Com2usProject/ReqResModel/BaseReqRes.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Com2usProject.ReqResModel;
-
-public class BaseReqRes
-{
-    public string AuthToken { get; set; }
-}
--- Com2usProject/Service/IRedisDb.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Com2usProject.Service;
-
-public interface IRedisDb : IDisposable
-{
-  
-    public Task<CSCommon.ErrorCode> AddAuthToken(string email, string token); // 토큰 추가하기
-
-    public Task<bool> CheckAuthTokenExist(string token);
-}
